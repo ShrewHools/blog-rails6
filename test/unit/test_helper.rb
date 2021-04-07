59d30ceee5608921a690fa81ce4f88b735aa1f06
shrewhools <vladyukha@yandex.by>
2021-04-07 20:19:06 +0300

    Add simplecov for unit

--- test/unit/test_helper.rb
+++ test/unit/test_helper.rb
@@ -1,3 +1,4 @@
+require 'simplecov'
 require 'minitest/autorun'
 require 'i18n'
 
@@ -7,4 +8,10 @@ load_paths += Dir[File.expand_path("#{app_root}/{app/models,lib}")]
 
 load_paths.each do |load_path|
   $LOAD_PATH.unshift(load_path) unless $LOAD_PATH.include?(load_path)
+end
+
+SimpleCov.start do
+  add_filter '/test/'
+  add_filter '/spec/'
+  add_filter '/config/'
 end
